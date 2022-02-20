0e2cd48778bd5287bd519ebcec7eaa914b69401a
AlBa231 <Oleksandr_Barabanov@epam.com>
2022-02-20 19:45:34 +0200

    feat(error): increase Toast timeout

--- S28Maker/Error/S28ToastErrorHandler.cs
+++ S28Maker/Error/S28ToastErrorHandler.cs
@@ -6,9 +6,11 @@ namespace S28Maker.Error
 {
     internal class S28ToastErrorHandler : S28ErrorHandler
     {
+        private const int ToastDurationMilliseconds = 10000;
+
         public override Task ShowError(string message)
         {
-            return Shell.Current.DisplayToastAsync(message);
+            return Shell.Current.DisplayToastAsync(message, ToastDurationMilliseconds);
         }
     }
 }
